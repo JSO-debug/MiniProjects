a1fea835c20fc8aa3b89a952088784e1dc118a8e
JSO-debug <oliveirajuan0509@gmail.com>
2022-02-01 17:48:29 +0000

    v0.001

---
+++ primeiro_codigo.py
@@ -0,0 +1,9 @@
+import selenium
+from time import sleep
+
+print("Esse código é apenas um teste")
+print("Testando", end='')
+for i in range(3):
+    print(i+1, end='')
+    sleep(1)
+print("\nAcabou")
